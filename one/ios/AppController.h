22bf4dbe1bcb650b699c34117c703b3827d26d64
Shinsuke Kasahara <obaqqqq@gmail.com>
2013-11-12 03:11:45 +0900

    Initial Commit

---
+++ one/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  oneAppController.h
+//  one
+//
+//  Created by 笠原 新介 on 13/11/12.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ one/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  oneAppController.h
+//  one
+//
+//  Created by 笠原 新介 on 13/11/12.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
